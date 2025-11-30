edd6a3d79263a17f1bfef2307f002ff77b6aa7e4
Demidog03 <olzhasktl362@gmail.com>
2025-11-30 07:54:03 +0500

    add: client api cors env variable

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8080',
+    origin: process.env.ALLOWED_CLIENT_API || 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
